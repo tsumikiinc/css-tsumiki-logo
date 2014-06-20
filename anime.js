3f6eafdfc6ab54ac9a163379df4c02046af82e8b
sato <sato@tsumikiinc.com>
2014-06-20 19:02:55 +0900

    Initial commit

---
+++ anime.js
@@ -0,0 +1,28 @@
+(function($){
+  // var $green = $('.cube-green');
+  // var $red = $('.cube-red');
+  // var $yellow = $('.cube-yellow');
+
+  // $green.velocity({
+  //   top: '+=30',
+  //   left: '+=30'
+  // }, {
+  //   /* Velocity's default options: */
+  //   duration: 1000,
+  //   easing: 'easeInOutElastic',
+  //   queue: '',
+  //   begin: function(elements) { console.log(elements[0]); },
+  //   progress: function(elements, percentComplete, timeRemaining, timeStart) {
+  //     console.log( (percentComplete * 100) + '%' );
+  //     console.log( timeRemaining + 'ms remaining!' );
+  //   },
+  //   complete: function(e) {
+  //     console.log(e);
+  //     t();
+  //   },
+  //   loop: 1,
+  //   delay: 500,
+  //   display: false,
+  //   mobileHA: true
+  // });
+})(jQuery);
